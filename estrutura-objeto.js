80d1fda3fd4751d5eadd3a90f4cfc18728caee47
AbimaelSantos8 <abimael.150@hotmail.com>
2026-04-29 10:49:56 -0300

    feat: criação e estrutura de objetos

---
+++ estrutura-objeto.js
@@ -0,0 +1,9 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12345678901',
+    turma: 'JavaScript'
+};
+
+console.log(estudante.nome);
+console.log(`os três primeiros numeros do cpf são ${estudante.cpf.substring(0, 3)}`);
---
+++ objeto.js
@@ -0,0 +1,14 @@
+//const listaCPFs = ['12345678911', '09876543212'];
+
+//const infosPessoa = ['nome', 'José Silva', 'idade', 32];
+
+//console.log(infosPessoa[1]);
+
+const objPessoa = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12345678901',
+    turma: 'JavaScript'
+};
+
+console.log(objPessoa);
