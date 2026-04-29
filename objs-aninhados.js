c380228d2d70f09770e3ec7eba3d7159c9ab0a8c
AbimaelSantos8 <abimael.150@hotmail.com>
2026-04-29 10:52:07 -0300

    feat: objetos em listas e objetos aninhados

---
+++ objs-aninhados.js
@@ -0,0 +1,16 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12345678901',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['5511983280197', '5511954340429']
+}
+
+estudante.endereco = {
+    Rua: 'Rua Domingos Nogueira',
+    numero : '335',
+    complemento: 'casa 2'
+}
+
+console.log(estudante.endereco.Rua);
